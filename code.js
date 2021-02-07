c423c91c9e6a3365a3a1434a44d850ab5b1667b6
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-02-07 16:54:00 +0200

    edited page

---
+++ code.js
@@ -0,0 +1,43 @@
+function Wellcome(){
+
+	var Pname = prompt("Please enter your name", "Nobody");
+
+
+
+ if (Pname == "Nobody") {
+  document.getElementById("welcome").innerHTML =
+  "Hey! tell me your name!";}
+
+else if (Pname != null) {
+  document.getElementById("welcome").innerHTML =
+  "Hello " + Pname + "! How are you today?";}
+
+}
+
+
+function AGE(){
+
+
+	var Age = prompt("How old are you?" , "Unknown");
+
+document.write("Your Age : " + Age);
+  
+}
+
+function Occupation(){
+
+var OC = prompt("What do you do?", "Nothing");
+
+
+
+ if (OC == "Nothing") {
+  document.write("Don't slack off!")}
+
+else if (OC != null) {
+
+	document.write("Occupation : " + OC);
+
+}
+
+
+}
