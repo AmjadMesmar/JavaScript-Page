5092f38807fc5857834a294af8f1b238aa222ce5
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-02-08 12:35:59 +0200

    added js code and css

---
+++ task2code.js
@@ -0,0 +1,39 @@
+function fimages ()
+{
+
+	var question = prompt("Sans or Papyrus ?");
+
+	while(question !== "Sans" && question !== "Papyrus"){
+
+		question = prompt("Sans or Papyrus ?");
+	}
+
+	var answer = '';
+
+	if (question == "Sans") 
+	{
+
+		answer = '<img src="Images/sans.gif"/>'
+
+		//document.write(answer);
+	}
+
+	else if (question == "Papyrus") 
+	{
+
+		answer = '<img src="Images/Papyrus.gif"/>'
+		// document.write(answer);
+
+	}
+
+	var inum = prompt("Write a number:")
+	var result ='';
+
+	for (var i =0; i<inum; i++){
+
+		result = result+ answer;
+
+	}
+	document.write(result);
+	
+}
